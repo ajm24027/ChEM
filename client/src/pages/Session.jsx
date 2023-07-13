cdd72ac126e36e1dbfe7f8ae02bee5485adf1c61
Anthony Medina <anthony.john.medina@gmail.com>
2023-07-12 20:21:26 -0400

    figured out sticky paper bottom for session input field

--- client/src/pages/Session.jsx
+++ client/src/pages/Session.jsx
@@ -9,6 +9,9 @@ import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import Typography from '@mui/material/Typography'
 import { Grid } from '@mui/material'
+import { positions } from '@mui/system'
+import { sizing } from '@mui/system'
+import Paper from '@mui/material/Paper'
 
 const Session = (props) => {
   console.log(props)
@@ -73,130 +76,30 @@ const Session = (props) => {
           </Typography>
         </Box>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 10 }}>
-        <Grid container spacing={1} direction="row">
-          <Grid xs={1.5}>
-            <Typography paragraph sx={{ marginRight: 2, opacity: 0.7 }}>
-              Username said:
-            </Typography>
-          </Grid>
-          <Grid xs={10.5}>
-            <Typography paragraph sx={{ opacity: 0.7 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              aliquet pellentesque suscipit. Aliquam vulputate massa vitae
-              pellentesque vehicula. Donec felis metus, commodo a rutrum id,
-              convallis quis ante. Suspendisse maximus neque viverra, efficitur
-              ante eget, ullamcorper ex. Sed dictum purus non magna volutpat,
-              sit amet rhoncus nisl elementum. Cras eget condimentum eros. Sed
-              rhoncus ornare sagittis. Proin in ipsum tempor, vehicula nisl
-              fringilla, ullamcorper neque. Fusce sit amet mollis ipsum. In hac
-              habitasse platea dictumst.{' '}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container spacing={1} direction="row">
-          <Grid xs={1.5}>
-            <Typography paragraph sx={{ marginRight: 2 }}>
-              Drake says:
-            </Typography>
-          </Grid>
-          <Grid xs={10.5}>
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              aliquet pellentesque suscipit. Aliquam vulputate massa vitae
-              pellentesque vehicula. Donec felis metus, commodo a rutrum id,
-              convallis quis ante. Suspendisse maximus neque viverra, efficitur
-              ante eget, ullamcorper ex. Sed dictum purus non magna volutpat,
-              sit amet rhoncus nisl elementum. Cras eget condimentum eros. Sed
-              rhoncus ornare sagittis. Proin in ipsum tempor, vehicula nisl
-              fringilla, ullamcorper neque. Fusce sit amet mollis ipsum. In hac
-              habitasse platea dictumst.{' '}
-            </Typography>
-          </Grid>
-        </Grid>
-        <CenterDivider>{date}</CenterDivider>
-        <Grid container spacing={1} direction="row">
-          <Grid xs={1.5}>
-            <Typography paragraph sx={{ marginRight: 2, opacity: 0.7 }}>
-              Username said:
-            </Typography>
-          </Grid>
-          <Grid xs={10.5}>
-            <Typography paragraph sx={{ opacity: 0.7 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              aliquet pellentesque suscipit. Aliquam vulputate massa vitae
-              pellentesque vehicula. Donec felis metus, commodo a rutrum id,
-              convallis quis ante. Suspendisse maximus neque viverra, efficitur
-              ante eget, ullamcorper ex. Sed dictum purus non magna volutpat,
-              sit amet rhoncus nisl elementum. Cras eget condimentum eros. Sed
-              rhoncus ornare sagittis. Proin in ipsum tempor, vehicula nisl
-              fringilla, ullamcorper neque. Fusce sit amet mollis ipsum. In hac
-              habitasse platea dictumst.{' '}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container spacing={1} direction="row">
-          <Grid xs={1.5}>
-            <Typography paragraph sx={{ marginRight: 2 }}>
-              Drake says:
-            </Typography>
-          </Grid>
-          <Grid xs={10.5}>
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              aliquet pellentesque suscipit. Aliquam vulputate massa vitae
-              pellentesque vehicula. Donec felis metus, commodo a rutrum id,
-              convallis quis ante. Suspendisse maximus neque viverra, efficitur
-              ante eget, ullamcorper ex. Sed dictum purus non magna volutpat,
-              sit amet rhoncus nisl elementum. Cras eget condimentum eros. Sed
-              rhoncus ornare sagittis. Proin in ipsum tempor, vehicula nisl
-              fringilla, ullamcorper neque. Fusce sit amet mollis ipsum. In hac
-              habitasse platea dictumst.{' '}
-            </Typography>
-          </Grid>
-        </Grid>
-        <CenterDivider>{date}</CenterDivider>
-        <Grid container spacing={1} direction="row">
-          <Grid xs={1.5}>
-            <Typography paragraph sx={{ marginRight: 2, opacity: 0.7 }}>
-              Username said:
-            </Typography>
-          </Grid>
-          <Grid xs={10.5}>
-            <Typography paragraph sx={{ opacity: 0.7 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              aliquet pellentesque suscipit. Aliquam vulputate massa vitae
-              pellentesque vehicula. Donec felis metus, commodo a rutrum id,
-              convallis quis ante. Suspendisse maximus neque viverra, efficitur
-              ante eget, ullamcorper ex. Sed dictum purus non magna volutpat,
-              sit amet rhoncus nisl elementum. Cras eget condimentum eros. Sed
-              rhoncus ornare sagittis. Proin in ipsum tempor, vehicula nisl
-              fringilla, ullamcorper neque. Fusce sit amet mollis ipsum. In hac
-              habitasse platea dictumst.{' '}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container spacing={1} direction="row">
-          <Grid xs={1.5}>
-            <Typography paragraph sx={{ marginRight: 2 }}>
-              Drake says:
-            </Typography>
-          </Grid>
-          <Grid xs={10.5}>
-            <Typography paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              aliquet pellentesque suscipit. Aliquam vulputate massa vitae
-              pellentesque vehicula. Donec felis metus, commodo a rutrum id,
-              convallis quis ante. Suspendisse maximus neque viverra, efficitur
-              ante eget, ullamcorper ex. Sed dictum purus non magna volutpat,
-              sit amet rhoncus nisl elementum. Cras eget condimentum eros. Sed
-              rhoncus ornare sagittis. Proin in ipsum tempor, vehicula nisl
-              fringilla, ullamcorper neque. Fusce sit amet mollis ipsum. In hac
-              habitasse platea dictumst.{' '}
-            </Typography>
-          </Grid>
-        </Grid>
-        <CenterDivider>{date}</CenterDivider>
+      {/* Non-Drawer Box */}
+      <Box component="main" sx={{ flexGrow: 1, overflow: 'auto' }}>
+        <Box></Box>
+        {/* Input Form Paper Field */}
+        <Paper
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            p: 2
+          }}
+          elevation={1}
+        >
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+            aliquet pellentesque suscipit. Aliquam vulputate massa vitae
+            pellentesque vehicula. Donec felis metus, commodo a rutrum id,
+            convallis quis ante. Suspendisse maximus neque viverra, efficitur
+            ante eget, ullamcorper ex. Sed dictum purus non magna volutpat, sit
+            amet rhoncus nisl elementum. Cras eget condimentum eros. Sed rhoncus
+            ornare sagittis. Proin in ipsum tempor, vehicula nisl fringilla,
+            ullamcorper neque. Fusce sit amet mollis ipsum. In hac habitasse
+            platea dictumst.
+          </Typography>
+        </Paper>
       </Box>
     </Box>
   )
