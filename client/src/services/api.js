3821846f74025135bf2bb1b3250dfd28f07314a4
Anthony Medina <anthony.john.medina@gmail.com>
2023-08-28 11:40:31 -0400

    updated BASE_URL endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001/'
+export const BASE_URL = 'https://chemchat-815dea929d70.herokuapp.com/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
